f4d0f2b80eceb0b4e32e2fd3b877d013991ebb9d
austinoduma9 <austinoduma9@gmail.com>
2025-03-28 15:47:36 +0300

    static files for production

--- core/settings.py
+++ core/settings.py
@@ -182,7 +182,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = [os.path.join( BASE_DIR,'docs')]
+STATICFILES_DIR = [os.path.join( BASE_DIR,'docs', 'static')]
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
